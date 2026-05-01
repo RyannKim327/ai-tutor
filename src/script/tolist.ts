a81483cdec9d3872f5c6bc6890327957e9ed6125
RyannKim327 <rksesgundo123@gmail.com>
2026-05-01 08:22:36 +0800

    05-01-26  8:22 [fixing: error in weekly challenge]

--- src/script/tolist.ts
+++ src/script/tolist.ts
@@ -1,6 +1,6 @@
 import { readFileSync, writeFileSync } from "fs"
 export default function add(week: number, topic: string) {
 	let data = readFileSync("data/lists.txt", "utf-8")
-	data += `Week ${week}: ${topic}\n`
+	data += ` ${topic}\nWeek - ${week + 1}:`
 	writeFileSync("data/lists.txt", data, "utf-8")
 }

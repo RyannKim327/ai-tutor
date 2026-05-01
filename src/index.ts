d4e480ffcb7c41fb2248f2b2fd3075551be24b97
RyannKim327 <rksesgundo123@gmail.com>
2026-05-01 08:03:34 +0800

    05-01-26  8:03 [adding facebook]

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import addChallenge from "./script/add-challenge"
 import ai from "./script/api"
 import extractor from "./script/extractor"
+import postToFacebook from "./script/posting"
 import add from "./script/tolist"
 
 async function main() {
@@ -10,6 +11,7 @@ async function main() {
 		if (ext.week_number === undefined)
 			throw Error("No Week")
 		add(ext.week_number, ext.challenge_name)
+		postToFacebook(`Week # ${ext.week_number} has arrived in ai-tutor, visit and explore another challenge from it thru: https://github.com/RyannKim327/ai-tutor/tree/main/week-${ext.week_number}`)
 		await addChallenge(ext)
 	} catch (e) {
 		main()
